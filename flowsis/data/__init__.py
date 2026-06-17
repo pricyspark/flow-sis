9c7790a468f3ab50c44948d9fd7f0a055d8a40a5
pricyspark <likev624@gmail.com>
2026-06-17 18:45:50 -0400

    Prototype overlap, zoom + crop, and photometric  augmentations.

--- flowsis/data/__init__.py
+++ flowsis/data/__init__.py
@@ -1,9 +1,28 @@
-from .augment import TransformDataset, AugmentationPipeline, roi_square_augment, rotation_augment
+from .augment import (
+    AugmentationPipeline,
+    AugmentationContext,
+    TransformDataset,
+    compute_focus_index,
+    overlap_augment,
+    photometric_augment,
+    roi_square_augment,
+    rotation_augment,
+    translate_augment,
+    zoom_crop_augment,
+    zoom_augment,
+)
 from .masks import load_binary, load_mask, mask2xywh
 
 __all__ = [
     "TransformDataset",
     "AugmentationPipeline",
+    "AugmentationContext",
+    "compute_focus_index",
+    "photometric_augment",
+    "translate_augment",
+    "zoom_crop_augment",
+    "zoom_augment",
+    "overlap_augment",
     "roi_square_augment",
     "rotation_augment",
     "load_binary",
