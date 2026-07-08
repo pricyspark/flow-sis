35717e040c6b7fcc8a38c6fd8e219fe78e22aaad
pricyspark <likev624@gmail.com>
2026-07-07 22:50:35 -0400

    Add extremely basic data setup script

---
+++ scripts/data.sh
@@ -0,0 +1,5 @@
+python flowsis/cli/data/create_classes.py
+python flowsis/cli/data/sample_keyframes.py
+python flowsis/cli/data/annotate_keyframes.py
+python flowsis/cli/data/build_hf_detection_dataset.py
+python flowsis/cli/data/build_hf_segmentation_dataset.py
